b42d0a6cba2717b0c61ed612f3fd58732030c8f4
gswack <gswack@aligntech.com>
2025-10-05 10:42:54 +0300

    Update in main.tf: directly entering ami ID to ami_id

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,7 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "aws_instance" "cloud-deploy" {
-  ami                    = var.ami_id
+  ami                    = "ami-03250b0e01c28d196"
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
